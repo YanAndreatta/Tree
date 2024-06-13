a6f480ee0ae5ee8a45b240dc421b3bbcdc1c350c
YanAndreatta <yan.duca2@gmail.com>
2024-06-13 17:10:28 -0300

    Estrutura de dados - Tree

---
+++ Tree/src/main/java/dataStructure/Tree.java
@@ -0,0 +1,107 @@
+package dataStructure;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Tree {
+
+  public Node root;
+
+  public static class Node {
+    public int value;
+    public Node left;
+    public Node right;
+
+    public Node(int value) {
+      this.value = value;
+    }
+
+    public boolean isLeaf() {
+      return (this.left == null) && (this.right == null);
+    }
+
+    public int getValue() {
+      return value;
+    }
+
+  }
+
+  public void insert(int value) {
+    if (root == null) root = new Node(value);
+    else {
+
+      Node newNode = new Node(value);
+      Queue<Node> queue = new LinkedList<>();
+      queue.add(root);
+
+      while (queue.size() > 0) {
+        Node currentNode = queue.remove();
+
+        // Verifica se o lado esquerdo é nulo 
+        if (currentNode.left == null) {
+          currentNode.left = newNode;
+          break;
+        } else {
+          queue.add(currentNode.left); // Incluí o lado esquerdo na Fila (filho esquerdo) caso já exista
+        }
+        
+        // Verifica se o lado direito é nulo
+        if (currentNode.right == null) {
+          currentNode.right = newNode;
+          break;
+        } else {
+          queue.add(currentNode.right); // Incluí o lado direito na Fila (filho direito) caso já exista
+        }
+      }
+    }
+  }
+
+  public void preOrder() {  // A Busca em Profundidade (DFS) - É exatamente o mesmo retorno da preOrder
+    preOrder(root);
+  }
+
+  private void preOrder(final Node node) {
+    // R E D
+    if (node == null) return;
+    System.out.println(node.value);
+    preOrder(node.left);
+    preOrder(node.right);
+  }
+
+  public void inOrder() {
+    inOrder(root);
+  }
+
+  private void inOrder(final Node node) {
+    // E R D
+    if (node == null) return;
+    inOrder(node.left);
+    System.out.println(node.value);
+    inOrder(node.right);
+  }
+
+  public void posOrder() {
+     posOrder(root);
+  }
+
+  private void posOrder(final Node node) {
+    // E D R
+    if (node == null) return;
+    posOrder(node.left);
+    posOrder(node.right);
+    System.out.println(node.value);
+  }
+
+  public void BFS() { // Implementado como travessia
+    if (root == null) return;
+    Queue<Node> queue = new LinkedList<>();
+    queue.add(root);
+    while(!queue.isEmpty()) {
+      Node node = queue.remove();
+      if (node.left != null) queue.add(node.left);
+      if (node.right != null) queue.add(node.right);
+
+      System.out.println(node.value);
+    }
+  }
+}
---
+++ Tree/src/main/java/main/App.java
@@ -0,0 +1,31 @@
+package main;
+
+import dataStructure.Tree;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+       Tree tree = new Tree();  
+       
+       tree.insert(37);
+       tree.insert(11);
+       tree.insert(66);
+       tree.insert(8);
+       tree.insert(17);
+       tree.insert(42);
+       tree.insert(72);
+
+       System.out.println("######################");
+       tree.BFS();
+       System.out.println("######################");
+
+    //    System.out.println(tree.root.value);
+    //    System.out.println(tree.root.left.value);
+    //    System.out.println(tree.root.right.value);
+
+    //    System.out.println(tree.root.isLeaf());
+    //    System.out.println(tree.root.right.isLeaf());
+    //    System.out.println(tree.root.left.isLeaf());
+    }
+}
